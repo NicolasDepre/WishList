ad1089ca39fc5d9f286238786e7528745e645a13
arfranck <arthur.franck@student.uclouvain.be>
2020-05-01 19:35:35 +0200

    see_wishes activity avec les views

---
+++ app/src/main/java/be/LaPireTeam/wishlist/SeeWishesActivity.java
@@ -0,0 +1,63 @@
+package be.LaPireTeam.wishlist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class SeeWishesActivity extends AppCompatActivity {
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_wishes_in_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.addNewWishButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create action for new list button
+                /*
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                 */
+            }
+        });
+
+        listView = (ListView)findViewById(R.id.see_my_wishes);
+
+        List l; //Liste passée en argument depuis l'activité MyListsActivity
+
+        Wish[] wishes;
+        wishes = l.getWishes();
+        ArrayList<String> myWishesNames = new ArrayList<>();
+        for(Wish w : wishes){
+            myWishesNames.add(w.getName());
+        }
+        //récupérer un arraylist de la base de données
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myWishesNames);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //i représente l'index de l'élément clické dans la view
+                //lancer activité see_wish particulière
+                return;
+            }
+        });
+    }
+
+}
