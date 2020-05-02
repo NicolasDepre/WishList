08ed89562cf8c41a3df25b0260143f8b1c676f72
arfranck <arthur.franck@student.uclouvain.be>
2020-05-02 14:52:49 +0200

    link button singup et login sur loginpage et registerpage

--- app/src/main/java/be/LaPireTeam/wishlist/MainActivity.java
+++ app/src/main/java/be/LaPireTeam/wishlist/MainActivity.java
@@ -1,5 +1,6 @@
 package be.LaPireTeam.wishlist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,12 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import be.LaPireTeam.wishlist.DAO.DAOFactory;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
     private static final int Connexion = 0;
     private static final int Inscription = 1;
-
 
     public Button[] listeBoutons = new Button[2];
 
@@ -30,7 +30,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 0; i < listeBoutons.length; i++) {
             listeBoutons[i].setOnClickListener(this);
         }*/
+        Button signUpButton = (Button) findViewById(R.id.signup_button_loginpage);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityInscription();
+            }
+        });
 
+    }
+
+    public void openActivityInscription(){
+        /*
+        Intent intentInscription = new Intent(this, MainActivity.InscriptionActivity.class);
+        startActivity(intentInscription);
+
+         */
+        setContentView(R.layout.register);
+    }
+
+    public void returnToLoginPage(){
+        setContentView(R.layout.login_page);
     }
 
     public void loginUser(View view){
@@ -43,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else usernameTxt.setText("UTILISATEUR LOGIN");
 
     }
-
+/*
     int activite;
 
     @Override
@@ -58,27 +78,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public class InscriptionActivity extends AppCompatActivity implements View.OnClickListener {
+ */
 
-        private static final int ValidInscription = 5;
-        public Button[] listeBoutons = new Button[1];
+    public class InscriptionActivity extends AppCompatActivity {
+
 
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
             setContentView(R.layout.register);
-            listeBoutons[ValidInscription] = ((Button) this.findViewById(R.id.ValidInscription));
-            for (int i = 0; i < listeBoutons.length; i++) {
-                listeBoutons[i].setOnClickListener(this);
-            }
         }
 
+        /*
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
             }
         }
-    }
 
+         */
+    }
+/*
     public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
         private static final int Wishlist = 2;
@@ -130,4 +150,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+
+ */
+
 }
+
+
