e9c4ccef6a731908dee7213d3aad771aea591039
arfranck <arthur.franck@student.uclouvain.be>
2020-05-04 17:09:17 +0200

    add page see detail wish

---
+++ app/src/main/java/be/LaPireTeam/wishlist/Activity/SeeDetailsWish.java
@@ -0,0 +1,14 @@
+package be.LaPireTeam.wishlist.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeeDetailsWish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_details_wish);
+    }
+}
