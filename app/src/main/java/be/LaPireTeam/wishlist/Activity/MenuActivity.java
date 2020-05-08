842a03c6f220bcef4fd06f573d800081b9dd6bf0
arfranck <arthur.franck@student.uclouvain.be>
2020-05-08 19:40:10 +0200

    documentation MenuActivity

--- app/src/main/java/be/LaPireTeam/wishlist/Activity/MenuActivity.java
+++ app/src/main/java/be/LaPireTeam/wishlist/Activity/MenuActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import be.LaPireTeam.wishlist.Objects.Session;
 import be.LaPireTeam.wishlist.R;
 
+/**
+ * Activité qui affiche le menu et qui gère les actions des boutons du menu
+ */
 public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +19,29 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.menu);
     }
 
+    /**
+     * Fonction appelée par le bouton "Profil" pour lancer l'activité pour afficher son profil
+     * @param view contient la view de là ou le bouton a été cliqué
+     */
     public void openProfilButton(View view) {
-        /*
-         *   Lance l'activity profil.
-         */
         Intent intent = new Intent(this, ProfilActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Fonction appelée par le bouton "Amis" pour lancer l'activité affichant les amis de l'utilisateur connecté
+     * @param view contient la view de là ou le bouton a été cliqué
+     */
     public void openAmisButton(View view) {
-        /*
-         *   Lance l'activity Amis.
-         */
         Intent intent = new Intent(this, AmisActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Fonction appelée par le bouton "Lists" pour lancer l'activité affichant les listes de l'utilisateur connecté
+     * @param view contient la view de là ou le bouton a été cliqué
+     */
     public void openListButton(View view) {
-        /*
-         *   Lance l'activity Mylists.
-         */
         Session.getInstance().setLastClickedFriend(null);
         Intent intent = new Intent(this, MyListsActivity.class);
         startActivity(intent);
